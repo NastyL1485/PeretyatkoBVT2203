f2d79a587812dc00953068eafc2620488fd3fd9c
NastyL1485 <peretyatkoanastasija@yandex.by>
2023-09-04 20:52:53 +0300

    новая ветка

--- laba 2.py
+++ laba 2.py
@@ -12,13 +12,3 @@ print("Минимальная температура:", round(data['main']['temp
 print("Максимальная температура:", round(data['main']['temp_max']))
 print("Скорость ветра", round(data['wind']['speed']))
 print("Видимость", round(data['visibility']))
-
-res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': APPID})
-data = res.json()
-
-
-print("Прогноз погоды на неделю:")
-for i in data['list']:
-    print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']), "> \r\nПогодные условия <", i['weather'][0]['description'], "> \r\nСкорость ветра <", round(i['wind']['speed']), "> \r\nВидимость <", round(i['visibility']))
-    print("____________________________")
