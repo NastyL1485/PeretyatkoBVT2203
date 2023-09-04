9c4a484c1e5d889a1fa7e1c0b8d71e8b9bcceaef
NastyL1485 <peretyatkoanastasija@yandex.by>
2023-09-04 21:05:30 +0300

    новая ветка

--- fgreretdg.py
+++ fgreretdg.py
@@ -1,10 +1,10 @@
 a = 1
 b = -5
 c = 9
-D = ((b**3)-(4*a*c))
+D = ((b**2)-(4*a*c))
 if D >= 0:
     x1 = ((-b+D**(1/2))/2*a)
     x2 = ((-b-D**(1/2))/2*a)
     print(x1, x2)
-
-print("Hello, World!")
+else:
+    print("Нет решения!")
